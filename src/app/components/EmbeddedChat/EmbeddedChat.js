6d38c18bf645999c7cc9d31e324d688edfa3244a
Franz Ogosi Cahuana <franz.ogosi@usil.pe>
2020-09-12 17:34:14 -0500

    closes #1; Fixing parent attribute on video and chat component

--- src/app/components/EmbeddedChat/EmbeddedChat.js
+++ src/app/components/EmbeddedChat/EmbeddedChat.js
@@ -2,12 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import { EmbeddedChatStyled } from "./EmbeddedChat.styles";
 const EmbeddedChat = ({ show, streamName }) => {
+  const { location } = document;
   return (
     <EmbeddedChatStyled show={show}>
       <iframe
         frameBorder="0"
         scrolling="no"
-        src={`https://www.twitch.tv/embed/${streamName}/chat?parent=localhost&darkpopout=true`}
+        src={`https://www.twitch.tv/embed/${streamName}/chat?parent=${location.hostname}&darkpopout`}
         height="100%"
         width="100%"
       ></iframe>
--- src/app/components/VideoPlayer/VideoPlayer.js
+++ src/app/components/VideoPlayer/VideoPlayer.js
@@ -3,30 +3,31 @@ import PropTypes from "prop-types";
 import { PlayerStyled } from "./VideoPlayer.styles";
 
 const VideoPlayer = ({ streamName, muted }) => {
-    const [player, setPlayer] = useState(null);
-    useEffect(() => {
-        setPlayer(
-            new Twitch.Player(streamName, {
-                width: "100%",
-                height: "100%",
-                channel: streamName,
-                parent: ["localhost"],
-            })
-        );
-    }, []);
+  const { location } = document;
+  const [player, setPlayer] = useState(null);
+  useEffect(() => {
+    setPlayer(
+      new Twitch.Player(streamName, {
+        width: "100%",
+        height: "100%",
+        channel: streamName,
+        parent: [location.hostname],
+      })
+    );
+  }, []);
 
-    useEffect(() => {
-        if (player != null) {
-            player.setMuted(muted);
-        }
-    }, [muted]);
+  useEffect(() => {
+    if (player != null) {
+      player.setMuted(muted);
+    }
+  }, [muted]);
 
-    return <PlayerStyled id={streamName}></PlayerStyled>;
+  return <PlayerStyled id={streamName}></PlayerStyled>;
 };
 
 VideoPlayer.propTypes = {
-    streamName: PropTypes.string.isRequired,
-    muted: PropTypes.bool.isRequired,
+  streamName: PropTypes.string.isRequired,
+  muted: PropTypes.bool.isRequired,
 };
 
 export default VideoPlayer;
